4321ba5afb9e9a780fcf408dea27b7e4e259fb09
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-03-28 11:28:05 +0200

    Inizializzazione repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,7 @@
         <main>
             <div class="container">
                 <div class="row">
-                    <div class="col text-center">
-
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
-
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
-                            </div>
-                        </div>
+                    <div class="col">
 
                     </div>
                 </div>
